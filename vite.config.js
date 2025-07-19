3322a54d8124392f469cdef24f1ff4b244e29eb4
JacquelineDeprey <jdeprey@terpmail.umd.edu>
2025-07-19 15:04:06 -0400

    take 5

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/withperpetua.github.io/',
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-}) 
+})
